eb5a53197e6cc49d01166153e734d6afcbb69c3c
gosumenep158b <go.sumenep158b@gmail.com>
2026-09-22 11:49:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Ganti dengan URL Web App Apps Script hasil deploy (Code.gs)
 // Contoh: https://script.google.com/macros/s/AKfycbx.../exec
-window.APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyhgmJRaDaI4UnE7exLDZgI-hANHc6m2opeygg8AMH8gF88RfqAXKFBf612Mkw-rZZj/exec";
+window.APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz4V-ZNI_9t-RLxKybRBaFAISNOAqdATyTbWk6g6HIT9JaTexCQLJqC06a_wjiQmYy2Ww/exec";
